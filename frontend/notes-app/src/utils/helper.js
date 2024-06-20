8f1245112f8e4ddb74d0dd752e30e7b87c747445
Himansu17 <kanhahimansu@gmail.com>
2024-06-20 21:21:24 +0530

    submit
    success

---
+++ frontend/notes-app/src/utils/helper.js
@@ -0,0 +1,30 @@
+export const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+};
+
+// export const getInitials = (name) => {
+//     if(!name) return "";
+
+//     const words = name.split("");
+//     let initials = "";
+
+//     for (let i = 0; i < Math.min(words.length, 2); i++){
+//         initials += words[i] [0];
+//     }
+//     return initials.toUpperCase();
+// }
+
+
+export const getInitials = (name) => {
+    if (!name || typeof name !== 'string') return "";
+
+    const words = name.split(" ");
+    let initials = "";
+
+    for (let i = 0; i < Math.min(words.length, 2); i++) {
+        initials += words[i][0];
+    }
+    return initials.toUpperCase();
+}
+
